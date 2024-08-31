3e6c5a65471750d8e30f5044a037147d20848ff9
BhavyaSreeJallolla <bhavyasreejallolla967@gmail.com>
2024-08-31 16:35:29 +0530

    Add COMPONENTS folder

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import Component from './'; 
+
+function App() {
+  return (
+    <div>
+      <Component />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+export function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 My Website</p>
+    </footer>
+  );
+}
